860c53c07af41e70aef37c2731fa051a3208312e
weiss-ben <weissb@ufl.edu>
2023-06-30 20:05:00 -0600

    Added ema

---
+++ src/main/technical_indicators/moving_averages.py
@@ -0,0 +1,37 @@
+'''
+Simple moving average (SMA)
+
+n : number of days to calculate sma over
+prices: data list of security prices
+'''
+def sma(n, prices):
+    sum = 0
+    
+    i = 0
+    for price in reversed(prices):
+        # Sum the prices for the specified number of days
+        sum += price
+        i += 1
+            
+        if i >= n:
+            break
+            
+    return sum / n
+    
+'''
+Exponential Moving Average (EMA)
+
+n : number of days to calculate sma over
+prices: data list of security prices
+'''
+def ema(n, prices):
+    s = 2 / (n + 1)
+    t = reversed(prices[0])
+    ema = 0
+
+    if n > 1:
+        ema += (t * s) + (ema(n - 1, prices[:-1]))
+    else:
+        return sma(n, prices)
+    
+    return ema
