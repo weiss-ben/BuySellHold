413d937b4a2ca5b38930af1812ca5a4614cd2dc8
weiss-ben <weissb@ufl.edu>
2023-07-01 23:15:54 -0600

    Added passing sma test

--- src/test/technical_indicators_test.py
+++ src/test/technical_indicators_test.py
@@ -1,11 +1,16 @@
-from main.technical_indicators import moving_averages
+from ..main.technical_indicators import moving_averages
 
 def run_tests():
     """
     This function executes the tests in the file
     """
 
+    # Technical Indicator Tests
     print('Executing technical indicators tests...')
+
+    # Moving Averages
+    print('Testing Simple Moving Average (SMA)...')
+    technical_indicators_test_1_sma()
 
 def technical_indicators_test_1_sma():
     # Test variables
@@ -13,8 +18,14 @@ def technical_indicators_test_1_sma():
     n = 1
 
     # Expected output 
-    
-    # Test output
-    test_sma_1 = sma(n, prices)
+    expected = 1
 
-    
+    # Test output
+    test_sma_1 = moving_averages.sma(n, prices)
+
+    if test_sma_1 == expected:
+        print('Passed: test_sma_1')
+    else:
+        print('Failed: Test test_sma_1')
+
+run_tests()
