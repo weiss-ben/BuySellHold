d81d51c655d0edf83343e21e0172500a841ef57d
weiss-ben <weissb@ufl.edu>
2023-07-02 20:39:17 -0600

    Added unittest

--- src/test/technical_indicators_test.py
+++ src/test/technical_indicators_test.py
@@ -1,31 +1,14 @@
+import unittest
 from ..main.technical_indicators import moving_averages
 
-def run_tests():
-    """
-    This function executes the tests in the file
-    """
+class TestMovingAverages(unittest.TestCase):
 
-    # Technical Indicator Tests
-    print('Executing technical indicators tests...')
+    # SMA Test
+    def test_sma(self):
+        # Valid
+        self.assertEqual(1, moving_averages.sma(1, [1]))
 
-    # Moving Averages
-    print('Testing Simple Moving Average (SMA)...')
-    technical_indicators_test_1_sma()
+        # In-valid
 
-def technical_indicators_test_1_sma():
-    # Test variables
-    prices = [1]
-    n = 1
-
-    # Expected output 
-    expected = 1
-
-    # Test output
-    test_sma_1 = moving_averages.sma(n, prices)
-
-    if test_sma_1 == expected:
-        print('Passed: test_sma_1')
-    else:
-        print('Failed: Test test_sma_1')
-
-run_tests()
+###### Run Tests #####
+unittest.main()
